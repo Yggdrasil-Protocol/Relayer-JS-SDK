20db59719c7fe0570356e3c3b503ff3be21c68c9
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2024-08-02 16:38:27 +0530

    chore: minor change

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { WS } from "./ws";
+// import { WS } from "./ws";
 
 export function hello(name: string): string {
     return `Hello ${name}`;
 }
 
-const ws = new WS.RelayerWS(["SPOT:BTC_USDT"]);
-ws.addListener("data", (e) => {
-    console.log(e);
-});
+// const ws = new WS.RelayerWS(["SPOT:BTC_USDT"]);
+// ws.addListener("data", (e) => {
+//     console.log(e);
+// });
 
-ws.addListener("info", (e) => {
-    console.info(e);
-});
+// ws.addListener("info", (e) => {
+//     console.info(e);
+// });
 
-setTimeout(() => {
-    ws.close();
-}, 10000);
+// setTimeout(() => {
+//     ws.close();
+// }, 10000);
 
-ws.connect();
+// ws.connect();
